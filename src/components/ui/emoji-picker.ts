7c951cb5568390336299b1d3342d6225b75912dd
Mohamad Mohebifar <netfars@gmail.com>
2020-06-17 21:12:18 -0400

    Fix emoji picker scroll state

--- src/components/ui/emoji-picker.ts
+++ src/components/ui/emoji-picker.ts
@@ -25,7 +25,7 @@ const icons: { [k in EmojiCategories]: Icons } = {
   "Travel & Places": Icons.Car,
   Activities: Icons.Sport,
   Objects: Icons.Lamp,
-  Flags: Icons.Flag
+  Flags: Icons.Flag,
 };
 
 export default class EmojiPicker implements Component<Options> {
@@ -42,7 +42,7 @@ export default class EmojiPicker implements Component<Options> {
       [129312, 129317],
       [129319, 129327],
       [128147, 128159],
-      [128162, 128166]
+      [128162, 128166],
     ],
     "Animals & Nature": [
       [127793, 127797],
@@ -51,11 +51,11 @@ export default class EmojiPicker implements Component<Options> {
       [128000, 128063],
       [129409, 129423],
       [129426, 129437],
-      [129445, 129449]
+      [129445, 129449],
     ],
     "Food & Drink": [
       [127812, 127871],
-      [129360, 129391]
+      [129360, 129391],
     ],
     "Travel & Places": [
       [9728, 9732],
@@ -66,14 +66,14 @@ export default class EmojiPicker implements Component<Options> {
       [128336, 128359],
       [128506, 128510],
       [128640, 128674],
-      [128676, 128680]
+      [128676, 128680],
     ],
     Activities: [
       [127878, 127883],
       [127885, 127889],
       [127917, 127924],
       [127951, 127955],
-      [129351, 129359]
+      [129351, 129359],
     ],
     Objects: [
       [127925, 127932],
@@ -85,8 +85,8 @@ export default class EmojiPicker implements Component<Options> {
       [128263, 128279],
       [129403, 129407],
       [129506, 129510],
-      [129529, 129534]
-    ]
+      [129529, 129534],
+    ],
   };
 
   public static emojis: { [k in EmojiCategories]: string[] };
@@ -118,7 +118,7 @@ export default class EmojiPicker implements Component<Options> {
     this.onEmojiSelect = onEmojiSelect;
 
     const element = createElement("div", {
-      class: styles.container
+      class: styles.container,
     });
 
     EmojiPicker.prepareEmojis();
@@ -132,7 +132,7 @@ export default class EmojiPicker implements Component<Options> {
       const emojis = createElement("div", { class: styles.emojisList });
       const title = createElement("h3", { class: styles.title }, category);
 
-      EmojiPicker.emojis[category].forEach(emoji => {
+      EmojiPicker.emojis[category].forEach((emoji) => {
         const button = createElement("button", { type: "button" });
         button.innerHTML = getEmojiImage(emoji);
         on(button, "click", () => {
@@ -156,26 +156,22 @@ export default class EmojiPicker implements Component<Options> {
       pickers.push(
         createElement(Icon, {
           icon: icons[category],
-          color: "grey"
+          color: "grey",
         })
       );
     });
 
-    const updateTabColor = throttle(
-      () => {
-        const { scrollTop } = emojiWrapper;
-        for (let i = titleElements.length - 1; i >= 0; i--) {
-          const titleElement = titleElements[i];
-          if (titleElement.offsetTop <= scrollTop) {
-            pickers.forEach(picker => picker.instance.setColor("grey"));
-            pickers[i].instance.setColor("blue");
-            break;
-          }
+    const updateTabColor = throttle(() => {
+      const { scrollTop } = emojiWrapper;
+      for (let i = categoryElements.length - 1; i >= 0; i--) {
+        const titleElement = categoryElements[i];
+        if (titleElement.offsetTop - 50 <= scrollTop) {
+          pickers.forEach((picker) => picker.instance.setColor("grey"));
+          pickers[i].instance.setColor("blue");
+          break;
         }
-      },
-      200,
-      true
-    );
+      }
+    }, 300, true);
 
     on(emojiWrapper, "scroll", updateTabColor);
     pickers[0].instance.setColor("blue");
@@ -188,7 +184,7 @@ export default class EmojiPicker implements Component<Options> {
         ...pickers.map((icon, i) => {
           const element = createElement("div", { class: "categoryTab" }, icon);
           on(element, "click", () => {
-            categoryElements[i].scrollIntoView({ behavior: "smooth" });
+            emojiWrapper.scrollTo({ top: categoryElements[i].offsetTop - 50, behavior: "smooth" });
           });
           return element;
         })
